ad7ceb8fbb14ee99585ad9b8a39ee2cb5a4edfe6
麻瓜 <your-email@example.com>
2026-05-27 06:27:16 +0800

    fix: 修复 PassWall 依赖包克隆
    - 使用 git clone 替代 svn co（GitHub SVN 支持已改变）
    - 从 openwrt-passwall-packages 仓库克隆所有依赖包
    - 修复缺失的依赖: ipt2socks, hysteria, naiveproxy, shadowsocks-rust 等

--- diy-script.sh
+++ diy-script.sh
@@ -36,37 +36,36 @@ svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/l
 # 科学上网插件
 git clone --depth 1 https://github.com/MilesPoupart/luci-app-vssr package/luci-app-vssr
 git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
+git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth 1 https://github.com/fw876/helloworld package/luci-app-ssr-plus-src
+mv package/luci-app-ssr-plus-src/luci-app-ssr-plus package/
+rm -rf package/luci-app-ssr-plus-src
 
-# 科学上网插件依赖
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/brook package/brook
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/dns2socks package/dns2socks
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/hysteria package/hysteria
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/microsocks package/microsocks
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/naiveproxy package/naiveproxy
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/ssocks package/ssocks
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/tcping package/tcping
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/trojan-go package/trojan-go
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/Openwrt-Passwall/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
-svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
-svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
-svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
-svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
-svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/redsocks2
+# PassWall 依赖包 (从 openwrt-passwall-packages 仓库克隆)
+git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/openwrt-passwall-packages
+# 从克隆的仓库中复制依赖包
+cp -r package/openwrt-passwall-packages/chinadns-ng package/
+cp -r package/openwrt-passwall-packages/dns2socks package/
+cp -r package/openwrt-passwall-packages/hysteria package/
+cp -r package/openwrt-passwall-packages/ipt2socks package/
+cp -r package/openwrt-passwall-packages/microsocks package/
+cp -r package/openwrt-passwall-packages/naiveproxy package/
+cp -r package/openwrt-passwall-packages/shadow-tls package/
+cp -r package/openwrt-passwall-packages/shadowsocks-libev package/
+cp -r package/openwrt-passwall-packages/shadowsocks-rust package/
+cp -r package/openwrt-passwall-packages/shadowsocksr-libev package/
+cp -r package/openwrt-passwall-packages/simple-obfs package/
+cp -r package/openwrt-passwall-packages/sing-box package/
+cp -r package/openwrt-passwall-packages/tcping package/
+cp -r package/openwrt-passwall-packages/trojan-plus package/
+cp -r package/openwrt-passwall-packages/tuic-client package/
+cp -r package/openwrt-passwall-packages/v2ray-geodata package/
+cp -r package/openwrt-passwall-packages/v2ray-plugin package/
+cp -r package/openwrt-passwall-packages/xray-core package/
+cp -r package/openwrt-passwall-packages/xray-plugin package/
+rm -rf package/openwrt-passwall-packages
 
 # Themes
 git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
